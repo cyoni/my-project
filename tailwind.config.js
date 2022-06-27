2632234978ca94eca2ce5b9dc9c401c88058ebbf
yoni <cyoni10@gmail.com>
2022-06-27 20:47:46 +0300

    first comit

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        twitter: '#00ADED'
+      }
+
+    },
   },
   plugins: [],
 }
